4d293985957e151fffa0fcfaac3862be19b9c716
ornanleonardo-creator <ornanleonardo@gmail.com>
2026-01-20 19:29:40 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/monapp/MainActivity.kt
+++ app/src/main/java/com/example/monapp/MainActivity.kt
@@ -1,32 +1,13 @@
 package com.example.monapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Lier l'écran du jeu
-        setContentView(R.layout.activity_main);
-
-        TextView scoreText = findViewById(R.id.scoreText);
-        Button tapButton = findViewById(R.id.tapButton);
-
-        tapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                score++;
-                scoreText.setText("Score : " + score);
-            }
-        });
+        setContentView(new GameView(this));
     }
 }
